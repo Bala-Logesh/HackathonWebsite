7667d0e17dafba56981b6646d781732c404950c3
Bala Logesh S <balassmp@gmail.com>
2021-09-12 21:17:41 +0530

    Added the account and cheques forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule, COMPONENTS } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AccountComponent } from './components/forms/account/account.component';
+import { ChequeComponent } from './components/forms/cheque/cheque.component';
+import { LendingComponent } from './components/forms/lending/lending.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent, COMPONENTS],
+  declarations: [AppComponent, NavbarComponent, FooterComponent, COMPONENTS, AccountComponent, ChequeComponent, LendingComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
